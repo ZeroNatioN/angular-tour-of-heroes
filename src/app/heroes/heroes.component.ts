edd81933311ae9d9073c433c780f37aeddf271ca
ZeroNatioN <michal.krause01@gmail.com>
2018-11-24 01:45:53 +0100

    Changed the hero component to get heroes data from hero service.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,15 +9,22 @@ import {HeroService} from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes:  Hero[];
   selectedHero: Hero;
+  
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  getHeroes(): void{
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
